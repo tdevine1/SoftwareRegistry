820485745964dde38d480e7f30725a9ea0bda6fd
davidpoling <soraspar78@gmail.com>
2016-03-31 23:55:10 -0400

    Tweaked the design by adding a toolbar with a "Software Finder" label on the top along with a back button, changed the colors and added a logo as-per FSU palette standard. Added some functionality with the keyboard typing so that resizing isn't an issue.
    Added a condition that if all of the fields are blank/don't have data selected, the user cannot submit a request.
    
    Database queries will follow after this commit...

--- SoftwareFinder/app/src/main/java/edu/fairmontstate/softwarefinder/LocationActivity.java
+++ SoftwareFinder/app/src/main/java/edu/fairmontstate/softwarefinder/LocationActivity.java
@@ -1,11 +1,17 @@
 package edu.fairmontstate.softwarefinder;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
 import android.widget.TextView;
 
-public class LocationActivity extends AppCompatActivity {
+public class LocationActivity extends ActionBarActivity {
+    Toolbar toolbar;
+    Drawable backArrow;
     Intent intent;
     TextView textView;
     String buildingItem;
@@ -15,6 +21,18 @@ public class LocationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location);
+
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        toolbar.setTitle("Software Finder");
+        toolbar.setTitleTextColor(Color.WHITE);
+        setSupportActionBar(toolbar);
+
+        backArrow = getResources().getDrawable(R.drawable.abc_ic_ab_back_mtrl_am_alpha);
+        backArrow.setColorFilter(getResources().getColor(R.color.white), PorterDuff.Mode.SRC_ATOP);
+        getSupportActionBar().setHomeAsUpIndicator(backArrow);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
 
         intent = getIntent();
         buildingItem = intent.getStringExtra(MainActivity.BUILDING_MSG);
--- SoftwareFinder/app/src/main/java/edu/fairmontstate/softwarefinder/ViewRequestActivity.java
+++ SoftwareFinder/app/src/main/java/edu/fairmontstate/softwarefinder/ViewRequestActivity.java
@@ -4,13 +4,20 @@
 package edu.fairmontstate.softwarefinder;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import java.util.ArrayList;
 
-public class ViewRequestActivity extends AppCompatActivity {
+public class ViewRequestActivity extends ActionBarActivity {
+    Toolbar toolbar;
+    Drawable backArrow;
     Intent intent;
     ListView listView;
     ArrayList<SoftwarePoint> requestList;
@@ -21,6 +28,18 @@ public class ViewRequestActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_request);
+
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        toolbar.setTitle("Software Finder");
+        toolbar.setTitleTextColor(Color.WHITE);
+        setSupportActionBar(toolbar);
+
+        backArrow = getResources().getDrawable(R.drawable.abc_ic_ab_back_mtrl_am_alpha);
+        backArrow.setColorFilter(getResources().getColor(R.color.white), PorterDuff.Mode.SRC_ATOP);
+        getSupportActionBar().setHomeAsUpIndicator(backArrow);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
 
         intent = getIntent();
 
